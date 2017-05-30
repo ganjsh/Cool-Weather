92ea5ce5ad7d5b96ee984d0f2f1099b7295d1850
ganjiasheng <ganjiasheng@yy.com>
2017-05-30 18:08:02 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/example/administrator/coolweather/model/County.java
@@ -0,0 +1,44 @@
+package com.example.administrator.coolweather.model;
+
+/**
+ * Created by Administrator on 2017/5/22.
+ */
+
+public class County {
+    private int id;
+    private String countyName;
+    private String countyCode;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCountyCode() {
+        return countyCode;
+    }
+
+    public void setCountyCode(String countyCode) {
+        this.countyCode = countyCode;
+    }
+}
---
+++ app/src/main/java/com/example/administrator/coolweather/util/Utility.java
@@ -0,0 +1,71 @@
+package com.example.administrator.coolweather.util;
+
+import android.text.TextUtils;
+
+import com.example.administrator.coolweather.model.City;
+import com.example.administrator.coolweather.model.CoolWeatherDB;
+import com.example.administrator.coolweather.model.County;
+import com.example.administrator.coolweather.model.Province;
+
+/**
+ * Created by sysu on 2017/5/30.
+ */
+
+public class Utility {
+
+    public synchronized static boolean handleProvincesResponse(CoolWeatherDB coolWeatherDB, String response){
+        if(!TextUtils.isEmpty(response)){
+            String[] allProvinces = response.split(",");
+            if(allProvinces != null && allProvinces.length > 0){
+                for(String p : allProvinces){
+                    String[] array = p.split("\\|");
+                    Province province = new Province();
+                    province.setProvinceCode(array[0]);
+                    province.setProvinceName(array[1]);
+
+                    coolWeatherDB.saveProvince(province);
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public synchronized static boolean handleCitiesResponse(CoolWeatherDB coolWeatherDB, String response, int provinceId){
+        if(!TextUtils.isEmpty(response)){
+            String[] allCities = response.split(",");
+            if(allCities != null && allCities.length > 0){
+                for(String c : allCities){
+                    String[] array = c.split("\\|");
+                    City city = new City();
+                    city.setCityCode(array[0]);
+                    city.setCityName(array[1]);
+                    city.setProvinceId(provinceId);
+
+                    coolWeatherDB.saveCity(city);
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public synchronized static boolean handleCountiesResponse(CoolWeatherDB coolWeatherDB, String response, int cityId){
+        if(!TextUtils.isEmpty(response)){
+            String[] allCounties = response.split(",");
+            if(allCounties != null && allCounties.length > 0){
+                for(String c : allCounties){
+                    String[] array = c.split("\\|");
+                    County county = new County();
+                    county.setCountyCode(array[0]);
+                    county.setCountyName(array[1]);
+                    county.setCityId(cityId);
+
+                    coolWeatherDB.saveCounty(county);
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+}
